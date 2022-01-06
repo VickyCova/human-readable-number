e74c43ff5f7ffa89292a3052a0e42cd6a1abc274
Victoria Belova <vacova1988@gmail.com>
2022-01-06 19:58:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+ module.exports = function toReadable(number) {
+    let str = number.toString();  
+    let num = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven', 'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    let ten = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    let hundred = 'hundred'; let summTwo = +(str[1] + str[2])    
+    
+    const firstNumber = str[0];
+    const secondNumber = str[1];
+    const thirdNumber = str[2];
+    
+    if(number === 0) {
+        return 'zero'
+    }
+
+    if (number < 20) {
+          return num[number]
+    }
+
+    if (number >=20 && number<100) {        
+         return (ten[firstNumber] + ' ' +  num[secondNumber]).trim()
+    }
+
+    if (number >=100 && number<1000 && summTwo >= 20) {
+        return (num[firstNumber] + ' ' + hundred + ' ' +  ten[secondNumber] + ' ' + num[thirdNumber]).trim()
+    }
+
+    if (number >=100 && number<1000 && summTwo < 20) {
+        return (num[firstNumber] + ' ' + hundred + ' ' +  num[summTwo]).trim()
+    }
 }
